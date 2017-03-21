7ce3259438f78ff0ae116247c89a5a6cde09da26
Homme Habile <colinste666@gmail.com>
2017-03-21 19:46:45 +0000

    le bluetooth fonctionne a merveille!

--- bluetooth/testBluetooth.cpp
+++ bluetooth/testBluetooth.cpp
@@ -6,23 +6,13 @@ int main() {
 	Bluetooth blue;
 	DDRB = 0xff;
 	PORTB = 0x00;
-	DDRD = 0x02;
+	DDRD |= 0x02;
 
-	PORTB = 0x02;
 	uint8_t reponse[200];
 	uint8_t size = blue.receiveString(reponse, 200);
-	PORTB = 0x01;
 
-	lcd.setBasicGraphics(1, 1, 0);
-	//lcd.writeString(reponse, size);
-	for (int i = 0; i < size; ++i)
-	{
-		lcd.writeByte(reponse[i]);
-	}
-
-	PORTB = 0x00;
-	if(reponse[0] != 'a')
-		PORTB = 0x00;
+	lcd.writeString(reponse, size);
+	lcd.setBasicGraphics(1, 0, 0);
 
 	for(;;) {
 		
